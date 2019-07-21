334c677fc5bd1b934b48a76bda4acc55c582f209
Cliff Harvey <cs.hbar@gmail.com>
2019-07-21 22:05:09 +0200

    Remove XML exception constructors

--- src/Microsoft/Translator/Exception.hs
+++ src/Microsoft/Translator/Exception.hs
@@ -3,16 +3,12 @@
 module Microsoft.Translator.Exception where
 
 import           Control.Exception
-import           Data.Text
 import           Data.Typeable
 import           Servant.Client
-import           Text.XML.Light.Types
 
 
 data TranslatorException
     = APIException ServantError
-    | InvalidXML Text
-    | UnexpectedXMLLayout Element
     | MissingSubscriptionKey
     deriving (Show, Typeable)
 
